49d8859fc000fc8526ca4e3a78768712ce2a6c7d
Jim O'Brien <jim@jimmed.net>
2018-09-30 12:31:02 +0100

    RemoteFile populates its own suggested filename and encoding on fetch

--- src/util/RemoteFile.js
+++ src/util/RemoteFile.js
@@ -1,10 +1,21 @@
 // @flow
 import fetch, { type FetchOptions } from "node-fetch";
+import { type Readable } from "stream";
 import TextFile, { ReadableFile } from "./TextFile";
+
+export function parseHeaderValue(value: string, keyToMatch: string): ?string {
+  const parts = value.split(/;\s*/g).map(part => part.split(/=/g));
+  const matchedPair = parts.find(([key, value]) => key === keyToMatch && value);
+  if (!matchedPair) return null;
+  const [, matchedValue] = matchedPair;
+  return matchedValue && matchedValue.replace(/(^['"]|['"]$)/g, "");
+}
 
 class RemoteTextFile<T> implements ReadableFile<T> {
   +url: string;
   +fetchOptions: ?FetchOptions;
+  suggestedFilename: ?string;
+  suggestedEncoding: ?string;
 
   static +parse: (rawValue: string) => Promise<T> | T;
 
@@ -20,7 +31,7 @@ class RemoteTextFile<T> implements ReadableFile<T> {
 
   read = TextFile.prototype.read;
 
-  async readRaw(): Promise<string> {
+  async fetch() {
     const response = await fetch(this.url, this.fetchOptions);
     if (!response.ok) {
       throw new Error(
@@ -29,7 +40,34 @@ class RemoteTextFile<T> implements ReadableFile<T> {
         }) was returned`
       );
     }
+    const contentType = response.headers.get("content-type");
+    if (contentType) {
+      const suggestedEncoding = parseHeaderValue(contentType, "charset");
+      if (suggestedEncoding) {
+        this.suggestedEncoding = suggestedEncoding.toLowerCase();
+      }
+    }
+    const contentDisposition = response.headers.get("content-dispositon");
+    if (contentDisposition) {
+      const suggestedFilename = parseHeaderValue(
+        contentDisposition,
+        "filename"
+      );
+      if (suggestedFilename) {
+        this.suggestedFilename = suggestedFilename;
+      }
+    }
+    return response;
+  }
+
+  async readRaw(): Promise<string> {
+    const response = await this.fetch();
     return response.text();
+  }
+
+  async readStream(): Promise<Readable> {
+    const response = await this.fetch();
+    return response.body;
   }
 }
 
